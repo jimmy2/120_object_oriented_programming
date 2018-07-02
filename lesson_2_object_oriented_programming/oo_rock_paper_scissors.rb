84176a7e2078c59696a996efa2d3c09c27f1967a
James Ferguson <jimmy2289@gmail.com>
2018-07-02 22:22:15 +0900

    Finished oo rock, paper, scissors game and walkthrough, with rubocop.

--- lesson_2_object_oriented_programming/oo_rock_paper_scissors.rb
+++ lesson_2_object_oriented_programming/oo_rock_paper_scissors.rb
@@ -2,58 +2,90 @@
 
 # Game Orchestration Engine
 
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == "scissors"
+  end
+
+  def rock?
+    @value == "rock"
+  end
+
+  def paper?
+    @value == "paper"
+  end
+
+  def >(other_move)
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
+  end
+
+  def to_s
+    @value
+  end
+end
+
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type=:human)
-    @player_type = player_type
-    @move = nil
-    set_name 
+  def initialize
+    set_name
   end
+end
 
+class Human < Player
   def set_name
-    if human?
-      n = ""
-      loop do
-        puts "What's your name?"
-        n = gets.chomp
-        break unless n.empty?
-        puts "Sorry, must enter a value?"
-      end
-      self.name = n
-    else
-      self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+    n = ""
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value?"
     end
+    self.name = n
   end
 
   def choose
-    if human?
-      choice = nil
-      loop do
-        puts "Please choose rock, paper, or scissors:"
-        choice = gets.chomp
-        break if ['rock', 'paper', 'scissors'].include?(choice)
-        puts "Sorry, invalid choice."
-      end
-      self.move = choice
-    else
-      self.move = ['rock', 'paper', 'scissors'].sample
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "Sorry, invalid choice."
     end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
   end
 
-  def human?
-    @player_type == :human
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
   end
-
 end
 
 class RPSGame
-
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome_message
@@ -64,22 +96,18 @@ class RPSGame
     puts "Thanks for playing Rock, Paper, Scissors! Goodbye!"
   end
 
-  def display_winner
+  def display_moves
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
-    case human.move
-    when "rock"
-      puts "It's a tie!" if computer.move == "rock"
-      puts "#{human.name} won!" if computer.move == "scissors"
-      puts "#{computer.name} won!" if computer.move == "paper"
-    when "paper"
-      puts "It's a tie!" if computer.move == "paper"
-      puts "#{human.name} won!" if computer.move == "rock"
-      puts "#{computer.name} won!" if computer.move == "scissors"
-    when "scissors"
-      puts "It's a tie!" if computer.move == "scissors"
-      puts "#{human.name} won!" if computer.move == "paper"
-      puts "#{computer.name} won!" if computer.move == "rock"
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
@@ -99,12 +127,12 @@ class RPSGame
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
     display_goodbye_message
   end
-
 end
 
 RPSGame.new.play
